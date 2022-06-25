23da7da91dd5a2a53fe69b8b510759a3013f9733
burakduman-bilgeadam <burkdumn@gmail.com>
2022-06-25 13:37:12 +0300

    add request mapp

--- src/main/java/com/example/firstSpringBoot/rest/GetMethodExample.java
+++ src/main/java/com/example/firstSpringBoot/rest/GetMethodExample.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/address",method = RequestMethod.GET)
 public class GetMethodExample {
     private Address address;
 
